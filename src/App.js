20f6b3317567f64c1eea6e46bc09086631d51d67
hsingwenhsu <b05502012@ntu.edu.tw>
2021-05-31 12:08:45 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
+import Web3 from 'web3';
+//import {TODO_LIST_ABI, TODO_LIST_ADDRESS} from './config'
+class App extends Component {
+  componentWillMount(){
+    this.loadBlockchainData()
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  async loadBlockchainData (){
+    const web3 = new Web3("http://localhost:8545")
+    const network = await web3.eth.net.getNetworkType()
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts)
+    this.setState({account: accounts[0]})
+    // const todoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS)
+    // this.setState({todoList})
+  }
+  constructor(props){
+    super(props)
+    this.state = {account:''}
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello world!</h1>
+        <p>Your account: {this.state.account}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,2 @@
+// export const TODO_LIST_ADDRESS = 'find this in the smart contract's json file'
+// export const TODO_LIST_ABI = [content of the json file]
